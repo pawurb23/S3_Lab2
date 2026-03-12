32be77f4c3317f2ed2a115b53cbba89dbb123d41
pawurb23 <pawel.urbaniak23@gmail.com>
2026-03-12 15:26:18 +0100

    Dodaj pliki projektów.

---
+++ Dane/Wynik.cs
@@ -0,0 +1,24 @@
+﻿namespace Dane
+{
+    public enum Styl
+    {
+        Dowolny,
+        Grzbietowy,
+        Klasyczny,
+        Motylkowy
+    }
+    public struct Wynik
+    {
+        public string Nazwisko { get; set; }
+        public int Dystans { get; set; }
+        public double Czas { get; set; }
+        public decimal Nagroda { get; set; }
+        public bool Rekord { get; set; }
+        public Styl StylPływania { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nazwisko}";
+        }
+    }
+}
---
+++ WpfAplikacja/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAplikacja
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
